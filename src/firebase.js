e7e35c81dc003971882a4c232b73cd1feffa570d
mwanzia14 <48007803+mwanzia14@users.noreply.github.com>
2025-04-05 12:30:37 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBEQBt7UK6u6dqBGVPS6GoEYHT7ftgeeHU",
-  authDomain: "myrecords-b9f07.firebaseapp.com",
-  projectId: "myrecords-b9f07",
-  storageBucket: "myrecords-b9f07.firebasestorage.app",
-  messagingSenderId: "373454993445",
-  appId: "1:373454993445:web:a1e6eb802715ff3deba7cc",
-  measurementId: "G-XVPL9FMKJD"
+  apiKey: "AIzaSyBdpYdqeKVnEltKpzIZHol1ZPN0Zu5hBX4",
+  authDomain: "my-records-d94f2.firebaseapp.com",
+  projectId: "my-records-d94f2",
+  storageBucket: "my-records-d94f2.firebasestorage.app",
+  messagingSenderId: "354724250611",
+  appId: "1:354724250611:web:83a130f0c4c807f2cac886",
+  measurementId: "G-DV6BPKD5DH"
 };
 
 const app = initializeApp(firebaseConfig);
